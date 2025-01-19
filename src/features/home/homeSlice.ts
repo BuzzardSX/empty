4e2aa0850db3efd32cd324fa9225f4524c4b2209
Павел Бобров <buzzard.sx@gmail.com>
2025-01-20 01:38:37 +0500

    open

--- src/features/home/homeSlice.ts
+++ src/features/home/homeSlice.ts
@@ -2,7 +2,9 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const homeSlice = createSlice({
 	name: 'home',
-	initialState: {},
+	initialState: {
+		userModalOpen: false
+	},
 	reducers: {}
 });
 
