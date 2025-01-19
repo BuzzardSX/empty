d602d043ba8cc6a6892165f4ea675956d878b169
Павел Бобров <buzzard.sx@gmail.com>
2025-01-20 01:45:15 +0500

    root

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Root } from './app';
 
 const app = document.getElementById('app');
 
-createRoot(app!).render(createElement('div'));
+const root = createElement(Root);
+
+createRoot(app!).render(root);
