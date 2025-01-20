cb22d015a8008c007f495d157467457a55bff409
BuzzardSX <buzzard.sx@gmail.com>
2025-01-20 10:33:08 +0500

    selector

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { useSelector } from 'react-redux';
 import rootReducer from './reducer';
 
 const store = configureStore({
 	reducer: rootReducer
 });
 
+type AppStore = typeof store;
+type RootState = ReturnType<AppStore['getState']>;
+
+export const useAppSelector = useSelector.withTypes<RootState>();
 export default store;
