33dbcd11530e8d616beb41c5b85d24b19db40a7e
BuzzardSX <buzzard.sx@gmail.com>
2025-01-22 15:50:29 +0500

    story

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,2 +1,3 @@
 export { default as Root } from './Root';
 export * from './store';
+export { default as store } from './store';
--- src/features/home/addUserButton/HomeAddUserButton.stories.tsx
+++ src/features/home/addUserButton/HomeAddUserButton.stories.tsx
@@ -1,8 +1,17 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { Provider } from 'react-redux';
+import { store } from '~/app';
 import { default as HomeAddUserButton } from './HomeAddUserButton';
 
 const meta: Meta<typeof HomeAddUserButton> = {
-	component: HomeAddUserButton
+	component: HomeAddUserButton,
+	decorators: [
+		(Story) => (
+			<Provider store={store}>
+				<Story />
+			</Provider>
+		)
+	]
 };
 
 export const primary: StoryObj<typeof HomeAddUserButton> = {
