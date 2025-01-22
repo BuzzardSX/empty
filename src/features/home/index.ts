b133c5d5f9ccae6679930ceced886ec9ff7ba096
BuzzardSX <buzzard.sx@gmail.com>
2025-01-22 15:59:04 +0500

    format

--- src/features/home/index.ts
+++ src/features/home/index.ts
@@ -1,4 +1,5 @@
 export { default as HomeAddUserButton } from './addUserButton';
 export { default as HomeAddUserModal } from './addUserModal';
+
 export * from './homeSlice';
 export { default } from './homeSlice';
