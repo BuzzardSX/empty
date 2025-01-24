21d3c76df08ac51bd04277edecfa41643723a56d
BuzzardSX <buzzard.sx@gmail.com>
2025-01-24 16:53:13 +0500

    page

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,10 +1,12 @@
 import { HomeAddUserButton, HomeAddUserModal } from '~/features/home';
+import { UserDescriptions } from '~/widgets';
 
 const Page = () => {
 	return (
 		<div>
 			<HomeAddUserButton type='primary' />
 			<HomeAddUserModal />
+			<UserDescriptions />
 		</div>
 	);
 };
