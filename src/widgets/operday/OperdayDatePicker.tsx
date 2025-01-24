32f58d735a2488d425c3a4a4603eac88cd1a38a6
Павел Бобров <buzzard.sx@gmail.com>
2025-01-24 22:57:59 +0500

    types

--- src/widgets/operday/OperdayDatePicker.tsx
+++ src/widgets/operday/OperdayDatePicker.tsx
@@ -1,8 +1,7 @@
 import { DatePicker } from 'antd';
 import { useGetOperdayAllQuery } from '~/api/emptyApi';
-import type { OperdayDatePickerProps } from './operdayWidgets';
 
-const OperdayDatePicker = ({ showCanceled = false }: OperdayDatePickerProps) => {
+const OperdayDatePicker = () => {
 	const { data } = useGetOperdayAllQuery();
 
 	return (
