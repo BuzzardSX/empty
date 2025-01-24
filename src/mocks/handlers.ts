06495f645341c42c8f97ed38edb67218d28e75c8
Павел Бобров <buzzard.sx@gmail.com>
2025-01-24 21:06:06 +0500

    operdays

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,13 @@
 import { http, HttpResponse } from 'msw';
 
 const handlers = [
-	http.get('/user', () => HttpResponse.json({ name: 'Mike', age: 19 }))
+	http.get('/user', () => HttpResponse.json({ name: 'Mike', age: 19 })),
+	http.get<never, never, Array<{ id: number, dateString: string }>>('/operday/all', () => HttpResponse.json([
+		{
+			id: 1,
+			dateString: '2024-02-01'
+		}
+	]))
 ];
 
 export default handlers;
