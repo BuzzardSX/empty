9c0a81d3c398401c0aac059c4b229f34014e14d1
BuzzardSX <buzzard.sx@gmail.com>
2025-01-24 16:41:20 +0500

    fetch

--- src/features/home/addUserButton/HomeAddUserButton.tsx
+++ src/features/home/addUserButton/HomeAddUserButton.tsx
@@ -1,9 +1,32 @@
 import { Button } from 'antd';
+import { useEffect, useState } from 'react';
 import type { HomeAddUserButtonProps } from '../homeFeature';
 import { useClickHandler } from './hooks';
 
+interface User {
+	name: string;
+	age: number;
+}
+
 const HomeAddUserButton = ({ type }: HomeAddUserButtonProps) => {
+	const [user, setUser] = useState<User>({ name: 'Initial.', age: 0 });
+
 	const clickHandler = useClickHandler();
+
+	useEffect(
+		() => {
+			const fetchData = async () => {
+				const response = await fetch('/user');
+
+				const json = (await response.json()) as User;
+
+				setUser(json);
+			};
+
+			fetchData();
+		},
+		[]
+	);
 
 	return (
 		<Button onClick={clickHandler} type={type}>
