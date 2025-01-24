61bcb09ca916d787b4fe8ddaf06b5994111b4b41
BuzzardSX <buzzard.sx@gmail.com>
2025-01-24 17:07:38 +0500

    api

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -1,6 +1,7 @@
 import { combineSlices } from '@reduxjs/toolkit';
+import emptyApi from '../../api/emptyApi';
 import home from '../../features/home';
 
-const rootReducer = combineSlices(home);
+const rootReducer = combineSlices(emptyApi, home);
 
 export default rootReducer;
