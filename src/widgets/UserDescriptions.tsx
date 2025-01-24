883f783d1b6395e9501cd9577c3fa91958c13dfa
BuzzardSX <buzzard.sx@gmail.com>
2025-01-24 17:25:23 +0500

    items

--- src/widgets/UserDescriptions.tsx
+++ src/widgets/UserDescriptions.tsx
@@ -1,16 +1,30 @@
 import { Descriptions } from 'antd';
 import type { DescriptionsItemType } from 'antd/es/descriptions';
+import { useGetUserQuery } from '~/api/emptyApi';
 
-const items: Array<DescriptionsItemType> = [
-	{
-		key: 'name',
-		label: 'Name',
-		children: 0
-	}
-]
+const useItems = () => {
+	const { data } = useGetUserQuery();
 
-const UserDescriptions = () => (
-	<Descriptions items={items} title='User info' />
-);
+	return [
+		{
+			key: 'name',
+			label: 'Name',
+			children: data?.name
+		},
+		{
+			key: 'age',
+			label: 'Age',
+			children: data?.age
+		}
+	] satisfies DescriptionsItemType[];
+};
+
+const UserDescriptions = () => {
+	const items = useItems();
+
+	return (
+		<Descriptions items={items} title='User info' />
+	);
+};
 
 export default UserDescriptions;
