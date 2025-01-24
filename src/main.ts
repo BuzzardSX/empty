2dd995299e05130088b9525c7acc9b88833cec37
BuzzardSX <buzzard.sx@gmail.com>
2025-01-24 15:21:11 +0500

    worker

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Root } from './app';
 
+async function enableMocking() {
+	const { worker } = await import('./mocks');
+
+	return worker.start();
+}
+
 const app = document.getElementById('app');
 
 const root = createElement(Root);
 
-createRoot(app!).render(root);
+enableMocking().then(() => createRoot(app!).render(root));
