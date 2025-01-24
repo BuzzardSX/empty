179b96b72c9d614836bea8e4bbc508a0a20ae663
BuzzardSX <buzzard.sx@gmail.com>
2025-01-24 18:10:35 +0500

    user

--- src/widgets/widgets.ts
+++ src/widgets/widgets.ts
@@ -1,4 +1,3 @@
 import { lazy } from 'react';
 
-export const UserDescriptions = lazy(() => import('./UserDescriptions'));
 export const UserDescriptionsSkeleton = lazy(() => import('./UserDescriptionsSkeleton'));
