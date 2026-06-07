0de498dfd5c2d3b9a1628fd66932cd6086dc1ddb
Павел Бобров <buzzard.sx@gmail.com>
2026-06-08 00:57:54 +0500

    list

---
+++ src/components/BigList.tsx
@@ -0,0 +1,18 @@
+interface Item {
+	id: number;
+	text: string;
+}
+
+interface Props {
+	items: Item[]
+}
+
+const BigList = ({ items }: Props) => (
+	<ul>
+		{items.map((i) => (
+			<li key={i.id}>{i.text}</li>
+		))}
+	</ul>
+);
+
+export default BigList;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
+export { default as BigList } from './BigList';
 export { default as Button } from './Button';
 export { default as TabContainer } from './TabContainer';
