16a4e9e2d62f96407c7756642b7b6a87c9c74c2b
Павел Бобров <buzzard.sx@gmail.com>
2026-06-08 00:46:41 +0500

    element

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
-import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
 
 const app = document.getElementById('app');
 
-createRoot(app!).render(createElement(App));
+createRoot(app!).render(<App />);
