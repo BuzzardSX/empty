d1129c478586efaf77ebd668988d8a0459526e85
BuzzardSX <buzzard.sx@gmail.com>
2026-06-10 01:08:40 +0500

    items

--- src/Page.tsx
+++ src/Page.tsx
@@ -2,18 +2,35 @@ import { BigListPlayground } from './components';
 import { Action } from './components/action';
 import { Panel } from './components/panel';
 
-const Page = () => (
-	<div>
+const panelItems = [
+	{
+		key: 1,
+		text: 'Act 1'
+	},
+	{
+		key: 2,
+		text: 'Act 2'
+	},
+	{
+		key: 3,
+		text: 'Act 3'
+	}
+];
+
+const Page = () => {
+	return (
 		<div>
-			<Panel items={[{ key: 1, text: 'Act' }]} />
+			<div>
+				<Panel items={panelItems} />
+			</div>
+			<div>
+				<Action>Act it</Action>
+			</div>
+			<div>
+				<BigListPlayground />
+			</div>
 		</div>
-		<div>
-			<Action>Act it</Action>
-		</div>
-		<div>
-			<BigListPlayground />
-		</div>
-	</div>
-);
+	);
+};
 
 export default Page;
