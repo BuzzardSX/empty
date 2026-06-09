9c27aed44bddb9fe2d41da1d60dbb9cd5520c529
BuzzardSX <buzzard.sx@gmail.com>
2026-06-10 00:52:11 +0500

    actions

--- src/components/panel/Panel.tsx
+++ src/components/panel/Panel.tsx
@@ -1,3 +1,11 @@
-const Panel = () => <div>Elements</div>;
+import { Action } from '~/components/action';
+
+const Panel = () => (
+	<div>
+		<Action>Act 1</Action>
+		<Action>Act 2</Action>
+		<Action>Act 3</Action>
+	</div>
+);
 
 export default Panel;
