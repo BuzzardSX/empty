ed0d0648bcbce3c5d89300522ce35dd25799e223
BuzzardSX <buzzard.sx@gmail.com>
2026-06-10 21:58:23 +0500

    incident

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,12 @@
-import Page from '../Page';
+// import { useId } from 'react';
+// import Page from '../Page';
 
 const App = () => {
-	return <Page />;
+	console.log('App rendered.');
+	// const id = useId();
+	// console.log('App id', id);
+
+	return <div>App</div>;
 }
 
 export default App;
