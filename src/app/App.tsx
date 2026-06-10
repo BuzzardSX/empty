219ad7c1a46271a5f54e6c3560cf778cd91ac9d1
BuzzardSX <buzzard.sx@gmail.com>
2026-06-10 21:59:47 +0500

    id

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,6 @@
-// import { useId } from 'react';
 // import Page from '../Page';
 
 const App = () => {
-	console.log('App rendered.');
-	// const id = useId();
-	// console.log('App id', id);
-
 	return <div>App</div>;
 }
 
