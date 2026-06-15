2dd86b52ff055a2ad13bd2b0d5c4fcc53d3623b9
BuzzardSX <buzzard.sx@gmail.com>
2026-06-16 00:42:21 +0500

    push

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,24 @@
-import { ReactNode } from 'react';
+import { type MouseEvent, ReactNode } from 'react';
 
 interface Props {
 	children: ReactNode;
 }
 
+const links: string[] = ['/', '/about'];
+
 const MainLayout = (p: Props) => {
+	const anchorClickHandler = (e: MouseEvent<HTMLAnchorElement>) => {
+		e.preventDefault();
+
+		history.pushState({}, '', '/about');
+	};
+
 	return (
 		<div>
 			<div>
-				<a href='/about'>About</a>
+				{links.map((l) => (
+					<a href={l} onClick={anchorClickHandler}>About</a>
+				))}
 			</div>
 			<div>{p.children}</div>
 		</div>
