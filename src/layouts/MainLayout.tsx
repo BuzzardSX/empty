1f673e100469ee0cabefcadf83f3c7c49cf3cb0f
BuzzardSX <buzzard.sx@gmail.com>
2026-06-16 00:46:35 +0500

    links

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,6 +6,21 @@ interface Props {
 
 const links: string[] = ['/', '/about'];
 
+interface LinkProps {
+	children: ReactNode;
+	href: string;
+}
+
+const Link = (p: LinkProps) => {
+	const anchorClickHandler = (e: MouseEvent<HTMLAnchorElement>) => {
+		e.preventDefault();
+
+		history.pushState({}, '', p.href);
+	};
+
+	return <a onClick={anchorClickHandler}>{p.children}</a>;
+};
+
 const MainLayout = (p: Props) => {
 	const anchorClickHandler = (e: MouseEvent<HTMLAnchorElement>) => {
 		e.preventDefault();
@@ -17,7 +32,7 @@ const MainLayout = (p: Props) => {
 		<div>
 			<div>
 				{links.map((l) => (
-					<a href={l} onClick={anchorClickHandler}>About</a>
+					<Link href={l}>{l}</Link>
 				))}
 			</div>
 			<div>{p.children}</div>
