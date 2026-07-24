40d8d78663dd12c1bc8399cf5ddcf5c7eb56b267
BuzzardSX <buzzard.sx@gmail.com>
2026-07-24 18:16:00 +0500

    action

--- src/features/product/UpdateForm.tsx
+++ src/features/product/UpdateForm.tsx
@@ -1,14 +1,22 @@
-const UpdateForm = () => (
-	<form>
-		<input type='hidden' />
-		<div>
-			<label>Артикул</label>
-			<input name='number' type='text' />
-		</div>
-		<div>
-			<button type='submit'>Добавить</button>
-		</div>
-	</form>
-);
+const UpdateForm = () => {
+	const action = (formData: FormData) => {
+		const product = Object.fromEntries(formData);
+
+		console.log(product);
+	};
+
+	return (
+		<form action={action}>
+			<input type='hidden' />
+			<div>
+				<label>Артикул</label>
+				<input name='number' type='text' />
+			</div>
+			<div>
+				<button type='submit'>Добавить</button>
+			</div>
+		</form>
+	);
+};
 
 export default UpdateForm;
