06d5b27f1bc229b207fbcb3dce4409ff8177c8d8
BuzzardSX <buzzard.sx@gmail.com>
2026-07-24 20:40:34 +0500

    target

--- src/features/product/UpdateForm.tsx
+++ src/features/product/UpdateForm.tsx
@@ -1,3 +1,5 @@
+import type { InputEventHandler } from 'react';
+
 const UpdateForm = () => {
 	const action = (formData: FormData) => {
 		const product = Object.fromEntries(formData);
@@ -5,12 +7,16 @@ const UpdateForm = () => {
 		console.log(product);
 	};
 
+	const inputHandler: InputEventHandler<HTMLInputElement> = (e) => {
+		console.log(e.currentTarget.value);
+	};
+
 	return (
 		<form action={action}>
 			<input type='hidden' name='number' />
 			<div>
 				<label>Артикул</label>
-				<input type='text' name='brandNumber' />
+				<input type='text' name='brandNumber' defaultValue={0} onInput={inputHandler} />
 			</div>
 			<div>
 				<button type='submit'>Добавить</button>
