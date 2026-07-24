4b5e63e1c36a89c53da58e52f8d90f787a19e272
BuzzardSX <buzzard.sx@gmail.com>
2026-07-24 18:07:11 +0500

    form

--- src/features/product/UpdateForm.tsx
+++ src/features/product/UpdateForm.tsx
@@ -1,14 +1,14 @@
-const UpdateForm = () => {
-	return (
-		<form>
-			<div>
-				<input name='name' type='text' />
-			</div>
-			<div>
-				<button type='submit'>Send</button>
-			</div>
-		</form>
-	);
-};
+const UpdateForm = () => (
+	<form>
+		<input type='hidden' />
+		<div>
+			<label>Артикул</label>
+			<input name='number' type='text' />
+		</div>
+		<div>
+			<button type='submit'>Добавить</button>
+		</div>
+	</form>
+);
 
 export default UpdateForm;
